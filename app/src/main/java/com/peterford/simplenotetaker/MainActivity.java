9dba25258f9f20aa8079a6421f80f3d5da9171b0
Peter Ford <pford90@gmail.com>
2018-02-18 13:56:14 -0500

    Added Toolbar with menu items, and partial sorts

--- app/src/main/java/com/peterford/simplenotetaker/MainActivity.java
+++ app/src/main/java/com/peterford/simplenotetaker/MainActivity.java
@@ -7,6 +7,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -23,7 +25,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.navigation_drawer);
         ButterKnife.bind(this);
         setSupportActionBar(mToolbar);
-        getSupportActionBar().setDisplayShowTitleEnabled(false);
+//        getSupportActionBar().setDisplayShowTitleEnabled(false);
 
         ActionBarDrawerToggle drawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout, mToolbar, R.string.open_drawer, R.string.close_drawer);
         mDrawerLayout.setDrawerListener(drawerToggle);
@@ -36,5 +38,38 @@ public class MainActivity extends AppCompatActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        String result = "";
+        switch (id) {
+            case R.id.menu_search:
+                result = "Search";
+                break;
+            case R.id.title_sort:
+                result = "Title Sort";
+                toggleMenuItems(item);
+                break;
+            case R.id.modified_date_asc:
+                result = "Modified Date";
+                toggleMenuItems(item);
+                break;
+            case R.id.menu_add:
+                result = "Add";
+                break;
+            default:
+                result = "Found no item";
+                break;
+        }
+
+        Toast.makeText(this, result, Toast.LENGTH_SHORT).show();
+        return true;
+    }
+
+    private void toggleMenuItems(MenuItem item) {
+        item.setChecked(!item.isChecked());
     }
 }
